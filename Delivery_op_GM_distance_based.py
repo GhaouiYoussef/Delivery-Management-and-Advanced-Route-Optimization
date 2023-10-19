e77cec1123de08d52122e193d3c8b7e7da82f9b6
youssef ghaoui <120917144+GhaouiYoussef@users.noreply.github.com>
2023-10-19 21:54:51 +0100

    Add files via upload
    Final_algorithm.py is the primary script that generates optimized route plans along with associated Google Maps links.
    
    Delivery_op_GM_distance_based.py houses the core functions responsible for performing the calculations necessary for task optimization.

--- Delivery_op_GM_distance_based.py
+++ Delivery_op_GM_distance_based.py
@@ -2,6 +2,28 @@ import random
 import requests
 from ortools.constraint_solver import pywrapcp, routing_enums_pb2
 
+def get_route_distances(locations, api_key):
+    url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+    num_locations = len(locations)
+    distances = [[0] * num_locations for _ in range(num_locations)]  # Initialize the distances matrix
+
+    for i in range(num_locations):
+        for j in range(i + 1, num_locations):
+            params = {
+                'key': api_key,
+                'origins': locations[i],
+                'destinations': locations[j],
+                'mode': 'driving',
+            }
+            response = requests.get(url, params=params)
+            data = response.json()
+
+            if 'rows' in data and len(data['rows']) > 0 and 'elements' in data['rows'][0] and 'distance' in data['rows'][0]['elements'][0]:
+                distance = data['rows'][0]['elements'][0]['distance']['value'] / 1000
+                distances[i][j] = distance
+                distances[j][i] = distance  # Distance matrix is symmetric
+
+    return distances
 
 def seconds_to_hms(seconds):
     hours = seconds // 3600
@@ -66,43 +88,54 @@ def genetic_algorithm(distance_matrix, num_routes, num_generations):
 
     best_route = min(population, key=lambda x: calculate_fitness(x, distance_matrix))
     return [0] + best_route + [0]  # Include the starting location (Firm)
+
+ 
 def solve_tsp_with_vehicles(data, distance_matrix, max_distance=1):
     max_distance = max_distance * 1000  # Convert max_distance to meters
-    # Modify the depot index to match the starting location in the route
     depot_index = 0
 
-    manager = pywrapcp.RoutingIndexManager(len(distance_matrix),
-                                           data['num_vehicles'], depot_index)
+    # Create a routing index manager with the number of locations, number of vehicles, and depot (starting location) index
+    manager = pywrapcp.RoutingIndexManager(len(distance_matrix), data['num_vehicles'], depot_index)
+
+    # Create a routing model
     routing = pywrapcp.RoutingModel(manager)
 
+    # Define a distance callback function that calculates the distance between two locations
     def distance_callback(from_index, to_index):
         from_node = manager.IndexToNode(from_index)
         to_node = manager.IndexToNode(to_index)
         return distance_matrix[from_node][to_node]
 
     transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Set the cost evaluator for all vehicles
     routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
 
+    # Configure search parameters
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.local_search_metaheuristic = (
-        routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH)
+    search_parameters.local_search_metaheuristic = routing_enums_pb2.LocalSearchMetaheuristic.GUIDED_LOCAL_SEARCH
     search_parameters.time_limit.seconds = 1
 
+    # Solve the TSP with the configured parameters
     solution = routing.SolveWithParameters(search_parameters)
+
     if solution:
         routes = []
         vehicle_id = data['num_vehicles'] - 1
         index = routing.Start(vehicle_id)
         route = []
+        
+        # Extract the routes for each vehicle
         while not routing.IsEnd(index):
             node = manager.IndexToNode(index)
             route.append(node)
             index = solution.Value(routing.NextVar(index))
-        ####################
-            # Modify the routes to start and end with the depot index
+        
+        # Modify the routes to start and end with the depot index
         routes = [[0] + route + [0] for route in routes]    
         routes.append(route)
 
+        # Calculate the total distance for each route
         total_distances = [
             sum(distance_matrix[route[j - 1]][route[j]] for j in range(1, len(route)))
             for route in routes
@@ -112,7 +145,7 @@ def solve_tsp_with_vehicles(data, distance_matrix, max_distance=1):
         best_route_idx = total_distances.index(min(total_distances))
         best_route_distance = total_distances[best_route_idx]
 
-        if best_route_distance > max_distance and data['num_vehicles'] >= 2:
+        if best_route_distance > max_distance or data['num_vehicles'] >= 2:
             # Split the best route into multiple routes if needed
             best_route = routes[best_route_idx]
             new_routes = split_route(best_route, distance_matrix, max_distance)
@@ -142,55 +175,17 @@ def split_route(route, distance_matrix, max_distance=1):
     sub_routes.append(sub_route)
     return sub_routes
 
-
-
-def get_route_distances(locations, api_key):
-    url = 'https://maps.googleapis.com/maps/api/distancematrix/json'
-    num_locations = len(locations)
-    distances = [[0] * num_locations for _ in range(num_locations)]  # Initialize the distances matrix
-
-    for i in range(num_locations):
-        for j in range(i + 1, num_locations):
-            params = {
-                'key': api_key,
-                'origins': locations[i],
-                'destinations': locations[j],
-                'mode': 'driving',
-            }
-            response = requests.get(url, params=params)
-            data = response.json()
-
-            if 'rows' in data and len(data['rows']) > 0 and 'elements' in data['rows'][0] and 'distance' in data['rows'][0]['elements'][0]:
-                distance = data['rows'][0]['elements'][0]['distance']['value'] / 1000
-                distances[i][j] = distance
-                distances[j][i] = distance  # Distance matrix is symmetric
-
-    return distances
+def generate_google_maps_link(locations):
+    base_url = "https://www.google.com/maps/dir/"
+    
+    # Convert each location to a format suitable for the URL
+    formatted_locations = [location.replace(' ', '+') for location in locations]
+    
+    # Combine the formatted locations to create the complete URL
+    full_url = base_url + '/'.join(formatted_locations)
+    
+    return full_url
 
 
 
-def optimize_delivery(firm, locations, GOOGLE_MAPS_API_KEY='AIzaSyAgaRnl5RlSg1bX79_CH3E3xchf_bgA6Gw', num_vehicles=2, max_distance=10):
-    locations.insert(0, firm)
-    index_to_location = {i: loc for i, loc in enumerate(locations)}
-    # print(f'index_to_location: {index_to_location}\n')
-    distance_matrix = get_route_distances(locations, GOOGLE_MAPS_API_KEY)  # Fetch distance matrix
-    data = create_data_model(locations, num_vehicles)
-    # print(f'data: {data}\n')
-    # print(f'distance_matrix: {distance_matrix}\n')
-    routes = solve_tsp_with_vehicles(data, distance_matrix, max_distance=max_distance)  # Use solve_tsp_with_vehicles function for optimization
-    # print(f'routes {routes}\n')
-    result = ""
 
-    if routes:
-        for i, route in enumerate(routes):
-            total_distance = sum(distance_matrix[route[j - 1]][route[j]] for j in range(1, len(route)))
-            # print(f'total_distance: {total_distance}\n')
-            # print(f'route: {route}\n')
-            
-            route_names = [index_to_location[loc_idx] for loc_idx in route]
-            result += f"Optimal Route for Vehicle {i + 1}: {' -> '.join(route_names)} \n"
-            result += f"Total Road Distance for Vehicle {i + 1}: {total_distance:.2f} kilometers \n\n"
-    else:
-        result = "No solution found."
-
-    return result
